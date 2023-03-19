7291a86d21081eee9a22bf9cc394348e33f864a6
Muhammd_Shariq_Dev <muhammadshariqsa@gmail.com>
2023-03-19 23:45:47 +0500

    configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
     CoffeeRatingModule,
