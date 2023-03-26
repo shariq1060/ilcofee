49ed1b861b86df1287641439fbc663bf7ed22e5d
Muhammd_Shariq_Dev <muhammadshariqsa@gmail.com>
2023-03-26 22:59:16 +0500

    custom config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,11 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(27017),
-      }),
+      load: [appConfig],
     }),
     CoffeesModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
